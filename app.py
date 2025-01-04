3ef34ee20577302660b6d023c40d0c7e13a84794
RaRe22 <ramcharancai22@gmail.com>
2025-01-04 12:13:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -976,6 +976,6 @@ def not_found_error(error):
 @app.errorhandler(500)
 def internal_error(error):
     return jsonify({"error": "Internal server error"}), 500
-
+    
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0',port=5000)
